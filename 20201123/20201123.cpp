dae5de4226ad58ff97b92e42c0c1826e9a6febcf
shakemaru <49559824+kyoujuro@users.noreply.github.com>
2020-11-23 10:39:00 +0900

    Create 20201123.cpp

---
+++ 20201123/20201123.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Rectangle{
+    public:
+    int Area() const{
+        return height_ * width_;
+    }
+    int height_;
+    int width_;
+};
+
+class Square: public Rectangle{
+    public:
+    void SetSize(int size){
+        height_ = size;
+        width_ = size;
+    }
+};
+
+int main(){
+  Square s;
+  s.SetSize(10);
+  std::cout << s.Area() << std::endl;
+  return 0;
+}
